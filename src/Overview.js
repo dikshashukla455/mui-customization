0af52f787e8a65537848ce5338189fc6b7ff013e
Diksha Shukla <deekshashukla03102001@gmail.com>
2023-03-02 01:46:40 +0530

    Added the tooltip component

--- src/Overview.js
+++ src/Overview.js
@@ -18,6 +18,7 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import CloseImg from "./images/cross.svg";
+import Tooltip from "@mui/material/Tooltip";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import LogoImg from "./images/Logo.jpg";
 import colorImg from "./images/color.png";
@@ -27,6 +28,7 @@ import fieldImg from "./images/field.png";
 import inputbtnImg from "./images/inputBtn.png";
 import dropdownImg from "./images/dropdown.png";
 import dialogImg from "./images/dialog.png";
+import tooltipImg from "./images/tooltip.png";
 import tabImg from "./images/tab.png";
 
 const ariaLabel = { "aria-label": "description" };
@@ -491,6 +493,59 @@ function Overview() {
 					}}
 				/>
 			</Tabs>
+			<Typography variant="h3" my={2}>
+				Tooltips
+			</Typography>
+			<Typography variant="bodyMedium">
+				The Tooltips can be customizable based on <br />
+				<br />
+				position of tooltips -
+				<ul>
+					<li>top-start</li>
+					<li>top(center)</li>
+					<li>top-end</li>
+					<li>left-start</li>
+					<li>left(center)</li>
+					<li>left-end</li>
+					<li>right-start</li>
+					<li>right(center)</li>
+					<li>right-end</li>
+					<li>bottom-start</li>
+					<li>bottom(center)</li>
+					<li>bottom-end</li>
+				</ul>
+				and using custom variant props through style overrides (MuiTooltip)
+				<br /> <br />
+				<img src={tooltipImg} width="600px" alt="" />
+				<br /> <br />
+			</Typography>
+			<Tooltip
+				title="This is a tooltip"
+				placement="top-start"
+				sx={{ margin: "10px" }}
+			>
+				<Button color="primary" variant="contained">
+					top-left tooltip
+				</Button>
+			</Tooltip>
+			<Tooltip
+				title="This is a tooltip"
+				placement="top"
+				sx={{ margin: "10px" }}
+			>
+				<Button color="primary" variant="contained">
+					top tooltip
+				</Button>
+			</Tooltip>
+			<Tooltip
+				title="This is a tooltip"
+				placement="top-end"
+				sx={{ margin: "10px" }}
+			>
+				<Button color="primary" variant="contained">
+					top-right tooltip
+				</Button>
+			</Tooltip>
 		</div>
 	);
 }
